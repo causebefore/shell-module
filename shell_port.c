739d66d11d5dc67c7bb515f9a5737f94b719ecf1
lubeiqu <lbq08@Foxmail.com>
2026-05-31 18:49:57 +0800

    feat(user): 创建shell_user.c，分离用户代码
    - 创建 shell_user.c，包含用户命令、变量、用户表
    - 从 shell_port.c 移除用户代码
    - shell_port.c 调用 shell_user_init() 初始化用户表

--- shell_port.c
+++ shell_port.c
@@ -94,105 +94,9 @@ void shell_uart3_irq_handler(void)
     }
 }
 
-/* ==================== 用户定义 ==================== */
-
+/* shell_user_init() 定义在 shell_user.c 中 */
 #if SHELL_USING_AUTH
-
-    #if SHELL_USING_HASH_PWD
-        /*
-         * 密码哈希值生成工具:
-         * 运行 shell_hash("your_password") 获取哈希值
-         * 然后将哈希值填入下方 password 字段
-         */
-        #define HASH_ROOT  0x7DD1705AUL /* hash("123456") */
-        #define HASH_ADMIN 0x0F12FC8EUL /* hash("admin")  */
-        #define HASH_NONE  0x00000000UL /* 无密码 */
-
-static const shell_user_t s_shell_users[] = {
-    {"root",  (const char*) (uintptr_t) HASH_ROOT,  SHELL_PERM_ROOT }, /* 超级用户 */
-    {"admin", (const char*) (uintptr_t) HASH_ADMIN, SHELL_PERM_ADMIN}, /* 管理员 */
-    {"guest", (const char*) (uintptr_t) HASH_NONE,  SHELL_PERM_USER }, /* 访客(无密码) */
-};
-    #else
-/* 明文密码方式 (不推荐) */
-static const shell_user_t s_shell_users[] = {
-    {"root",  "123456", SHELL_PERM_ROOT }, /* 超级用户 */
-    {"admin", "admin",  SHELL_PERM_ADMIN}, /* 管理员 */
-    {"guest", "",       SHELL_PERM_USER }, /* 访客(无密码) */
-};
-    #endif
-
-    #define USER_COUNT (sizeof(s_shell_users) / sizeof(s_shell_users[0]))
-#endif
-
-/* ==================== 用户命令 ==================== */
-
-static int cmd_test(int argc, char* argv[])
-{
-    shell_print(g_shell, "Test OK\r\n");
-    return 0;
-}
-
-/* 需要管理员权限的命令示例 */
-static int cmd_reboot(int argc, char* argv[])
-{
-    shell_print(g_shell, "System rebooting...\r\n");
-    /* HAL_NVIC_SystemReset(); */
-    return 0;
-}
-
-/* 带参数补全的命令示例 */
-static int cmd_mode(int argc, char* argv[])
-{
-    if (argc < 2)
-    {
-        shell_print(g_shell, "Usage: mode <speed|angle|torque>\r\n");
-        return -1;
-    }
-    shell_printf(g_shell, "Mode set to: %s\r\n", argv[1]);
-    return 0;
-}
-
-/* 补全列表 (必须以 NULL 结尾) */
-static const char* s_mode_opts[] = {"speed", "angle", "torque", NULL};
-
-/* ==================== 宏注册命令 ==================== */
-
-/* 内置命令 */
-SHELL_EXPORT_CMD(help, "Show commands", cmd_help, SHELL_PERM_NONE);
-SHELL_EXPORT_CMD(clear, "Clear screen", cmd_clear, SHELL_PERM_NONE);
-#if SHELL_USING_HISTORY
-SHELL_EXPORT_CMD(history, "Show history", cmd_history, SHELL_PERM_NONE);
-#endif
-#if SHELL_USING_VAR
-SHELL_EXPORT_CMD(var, "Read/write variable", cmd_var, SHELL_PERM_NONE);
-SHELL_EXPORT_CMD(vars, "List all variables", cmd_vars, SHELL_PERM_NONE);
-#endif
-
-#if SHELL_USING_AUTH
-SHELL_EXPORT_CMD(login, "Login user", cmd_login, SHELL_PERM_NONE);
-SHELL_EXPORT_CMD(logout, "Logout", cmd_logout, SHELL_PERM_NONE);
-SHELL_EXPORT_CMD(whoami, "Current user", cmd_whoami, SHELL_PERM_NONE);
-#endif
-
-/* 用户命令 */
-SHELL_EXPORT_CMD(test, "Test command", cmd_test, SHELL_PERM_USER);                   /* 需要登录 */
-SHELL_EXPORT_CMD(reboot, "System reboot", cmd_reboot, SHELL_PERM_ADMIN);             /* 需要管理员 */
-SHELL_EXPORT_CMD_LIST(mode, "Set FOC mode", cmd_mode, SHELL_PERM_NONE, s_mode_opts); /* 带参数补全 */
-
-/* ==================== 示例变量导出 ==================== */
-#if SHELL_USING_VAR
-static int         s_test_int   = 100;
-static uint32_t    s_test_uint  = 0x12345678;
-static float       s_test_float = 3.14f;
-static uint8_t     s_test_bool  = 1;
-static const char* s_version    = "1.0.0";
-
-SHELL_EXPORT_VAR(test_int, &s_test_int, SHELL_VAR_INT);
-SHELL_EXPORT_VAR(test_uint, &s_test_uint, SHELL_VAR_UINT);
-SHELL_EXPORT_VAR(test_float, &s_test_float, SHELL_VAR_FLOAT);
-SHELL_EXPORT_VAR(test_bool, &s_test_bool, SHELL_VAR_BOOL);
-SHELL_EXPORT_VAR_RO(version, &s_version, SHELL_VAR_STRING);
+extern void shell_user_init(shell_t* sh);
 #endif
 
 /* ==================== 初始化 ==================== */
@@ -208,8 +112,8 @@ void my_shell_init(void)
     g_shell_usart3.read = shell_read_impl;
 
 #if SHELL_USING_AUTH
-    /* 设置用户列表 */
-    shell_set_users(&g_shell_usart3, s_shell_users, USER_COUNT);
+    /* 设置用户列表 (用户表定义在 shell_user.c) */
+    shell_user_init(&g_shell_usart3);
 #endif
 
     /* 启用UART3接收中断 */
---
+++ shell_user.c
@@ -0,0 +1,107 @@
+/**
+ * @file    shell_user.c
+ * @brief   用户命令、变量、用户表
+ */
+
+#include "shell.h"
+
+/* ==================== 用户命令示例 ==================== */
+
+static int cmd_test(int argc, char* argv[])
+{
+    shell_print(g_shell, "Test OK\r\n");
+    return 0;
+}
+
+static int cmd_reboot(int argc, char* argv[])
+{
+    shell_print(g_shell, "System rebooting...\r\n");
+    /* HAL_NVIC_SystemReset(); */
+    return 0;
+}
+
+static int cmd_mode(int argc, char* argv[])
+{
+    if (argc < 2)
+    {
+        shell_print(g_shell, "Usage: mode <speed|angle|torque>\r\n");
+        return -1;
+    }
+    shell_printf(g_shell, "Mode set to: %s\r\n", argv[1]);
+    return 0;
+}
+
+/* 补全列表 */
+static const char* s_mode_opts[] = {"speed", "angle", "torque", NULL};
+
+/* ==================== 宏注册命令 ==================== */
+
+/* 内置命令 */
+SHELL_EXPORT_CMD(help, "Show commands", cmd_help, SHELL_PERM_NONE);
+SHELL_EXPORT_CMD(clear, "Clear screen", cmd_clear, SHELL_PERM_NONE);
+#if SHELL_USING_HISTORY
+SHELL_EXPORT_CMD(history, "Show history", cmd_history, SHELL_PERM_NONE);
+#endif
+#if SHELL_USING_VAR
+SHELL_EXPORT_CMD(var, "Read/write variable", cmd_var, SHELL_PERM_NONE);
+SHELL_EXPORT_CMD(vars, "List all variables", cmd_vars, SHELL_PERM_NONE);
+#endif
+
+#if SHELL_USING_AUTH
+SHELL_EXPORT_CMD(login, "Login user", cmd_login, SHELL_PERM_NONE);
+SHELL_EXPORT_CMD(logout, "Logout", cmd_logout, SHELL_PERM_NONE);
+SHELL_EXPORT_CMD(whoami, "Current user", cmd_whoami, SHELL_PERM_NONE);
+#endif
+
+/* 用户命令 */
+SHELL_EXPORT_CMD(test, "Test command", cmd_test, SHELL_PERM_USER);
+SHELL_EXPORT_CMD(reboot, "System reboot", cmd_reboot, SHELL_PERM_ADMIN);
+SHELL_EXPORT_CMD_LIST(mode, "Set FOC mode", cmd_mode, SHELL_PERM_NONE, s_mode_opts);
+
+/* ==================== 用户表 ==================== */
+
+#if SHELL_USING_AUTH
+
+#if SHELL_USING_HASH_PWD
+#define HASH_ROOT  0x7DD1705AUL /* hash("123456") */
+#define HASH_ADMIN 0x0F12FC8EUL /* hash("admin")  */
+#define HASH_NONE  0x00000000UL /* 无密码 */
+
+static const shell_user_t s_shell_users[] = {
+    {"root",  (const char*) (uintptr_t) HASH_ROOT,  SHELL_PERM_ROOT },
+    {"admin", (const char*) (uintptr_t) HASH_ADMIN, SHELL_PERM_ADMIN},
+    {"guest", (const char*) (uintptr_t) HASH_NONE,  SHELL_PERM_USER },
+};
+#else
+static const shell_user_t s_shell_users[] = {
+    {"root",  "123456", SHELL_PERM_ROOT },
+    {"admin", "admin",  SHELL_PERM_ADMIN},
+    {"guest", "",       SHELL_PERM_USER },
+};
+#endif
+
+#define USER_COUNT (sizeof(s_shell_users) / sizeof(s_shell_users[0]))
+
+/* 设置用户表的便捷函数 */
+void shell_user_init(shell_t* sh)
+{
+    shell_set_users(sh, s_shell_users, USER_COUNT);
+}
+
+#endif /* SHELL_USING_AUTH */
+
+/* ==================== 变量导出示例 ==================== */
+
+#if SHELL_USING_VAR
+static int         s_test_int   = 100;
+static uint32_t    s_test_uint  = 0x12345678;
+static float       s_test_float = 3.14f;
+static uint8_t     s_test_bool  = 1;
+static const char* s_version    = "1.0.0";
+
+SHELL_EXPORT_VAR(test_int, &s_test_int, SHELL_VAR_INT);
+SHELL_EXPORT_VAR(test_uint, &s_test_uint, SHELL_VAR_UINT);
+SHELL_EXPORT_VAR(test_float, &s_test_float, SHELL_VAR_FLOAT);
+SHELL_EXPORT_VAR(test_bool, &s_test_bool, SHELL_VAR_BOOL);
+SHELL_EXPORT_VAR_RO(version, &s_version, SHELL_VAR_STRING);
+#endif
